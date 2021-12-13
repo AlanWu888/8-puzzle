de5ef85616dc314127efc6913adfee16c77c44c6
AlanWu888 <74883696+AlanWu888@users.noreply.github.com>
2021-12-13 17:33:11 +0000

    Now writes to txt

--- dfs_main.java
+++ dfs_main.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
+import java.io.IOException;
+import java.io.FileWriter;
 
 public class dfs_main {
 	public static void main(String[] args) {
@@ -14,20 +17,56 @@ public class dfs_main {
 	    System.out.println("Enter second state (S2): ");
 	    String S2 = myObj.nextLine();
 	    
+	    
+	    
 	    System.out.println("S1:");    
 	    dfs_search searchS1 = new dfs_search(new Node(format(S1)));
 	    Set<String> RS1 = new HashSet<>(searchS1.depthFirstSearch());	// set of S1's states as set
-	    List<String> listS1 = new ArrayList<String>();					// set -> list
-	    listS1.addAll(RS1);
 	    
 	    System.out.println("S2:");    
 	    dfs_search searchS2 = new dfs_search(new Node(format(S2)));
 	    Set<String> RS2 = new HashSet<>(searchS2.depthFirstSearch());	// set if S2's states as set
 
+	    writeToTextFile(S1, S2, RS1, RS2); 
 	    
 	    System.out.println("Getting similarities:");					// compare the sets
-	    System.out.println(findIntersection(RS1, RS2).size());
+	    System.out.println(findIntersection(RS1, RS2).size());   
     }
+	
+	private static void writeToTextFile(String input1, String input2, Set<String> rS1, Set<String> rS2) {
+		 try {
+		      FileWriter myWriter = new FileWriter("RESULTS.txt");
+		      myWriter.write("TAG First state:  " + input1 + "     states: "+ rS1.size() + '\n');
+		      Iterator<String> it_1 = rS1.iterator();
+		      while(it_1.hasNext()){
+		    	  myWriter.write(it_1.next() + " ");
+		      }
+
+		      myWriter.write(" " + '\n'+ '\n'+ '\n'+ '\n');
+		      
+		      myWriter.write("TAG Second state: " + input2 + "     states: "+ rS2.size() + '\n');
+		      Iterator<String> it_2 = rS2.iterator();
+		      while(it_2.hasNext()){
+		    	  myWriter.write(it_2.next() + " ");
+		      }
+		      
+		      myWriter.write(" " + '\n'+ '\n'+ '\n'+ '\n');
+		      
+		      
+		      Set<String> similaritiesSet = findIntersection(rS1, rS2);
+		      myWriter.write("TAG Matches found: " + similaritiesSet.size() + '\n');
+		      Iterator<String> it_intersection = rS2.iterator();
+		      while(it_intersection.hasNext()){
+		    	  myWriter.write(it_intersection.next() + " ");
+		      }
+		      
+		      myWriter.close();
+		      System.out.println("Successfully wrote to the file.");
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
 	
 	private static Set<String> findIntersection(Set<String> rS1, Set<String> rS2) {
 		rS1.retainAll(rS2);
