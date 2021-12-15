0069f9dad54fe4c3b9edbcb7879838eeb7ef92be
AlanWu888 <74883696+AlanWu888@users.noreply.github.com>
2021-12-15 10:07:05 +0000

    Update Node.java
    Added comments

--- Node.java
+++ Node.java
@@ -32,60 +32,60 @@ public class Node {
         List<String> children = new ArrayList<String>();
 
         switch (parent.indexOf("0")) {
-            case 0: {
-            	/*
+                /*
             	 * replace the character at the index of 0, where the position of the blank tile has been found, with a temporary
             	 * character (#), and then replace this with the character in adjacent space to blank tile
             	 */
-                children.add(parent.replace(parent.charAt(0), '#').replace(parent.charAt(1), parent.charAt(0)).replace('#', parent.charAt(1)));
-                children.add(parent.replace(parent.charAt(0), '#').replace(parent.charAt(3), parent.charAt(0)).replace('#', parent.charAt(3)));
+            case 0: {   // Blank located at top left
+                children.add(parent.replace(parent.charAt(0), '#').replace(parent.charAt(1), parent.charAt(0)).replace('#', parent.charAt(1))); // Move Right
+                children.add(parent.replace(parent.charAt(0), '#').replace(parent.charAt(3), parent.charAt(0)).replace('#', parent.charAt(3))); // Move Down
                 break;
             }
-            case 1: {
-                children.add(parent.replace(parent.charAt(1), '#').replace(parent.charAt(0), parent.charAt(1)).replace('#', parent.charAt(0)));
-                children.add(parent.replace(parent.charAt(1), '#').replace(parent.charAt(2), parent.charAt(1)).replace('#', parent.charAt(2)));
-                children.add(parent.replace(parent.charAt(1), '#').replace(parent.charAt(4), parent.charAt(1)).replace('#', parent.charAt(4)));
+            case 1: {   // Blank located at top middle
+                children.add(parent.replace(parent.charAt(1), '#').replace(parent.charAt(0), parent.charAt(1)).replace('#', parent.charAt(0))); // Move Left
+                children.add(parent.replace(parent.charAt(1), '#').replace(parent.charAt(2), parent.charAt(1)).replace('#', parent.charAt(2))); // Move Right
+                children.add(parent.replace(parent.charAt(1), '#').replace(parent.charAt(4), parent.charAt(1)).replace('#', parent.charAt(4))); // Move Down
                 break;
             }
-            case 2: {
-                children.add(parent.replace(parent.charAt(2), '#').replace(parent.charAt(1), parent.charAt(2)).replace('#', parent.charAt(1)));
-                children.add(parent.replace(parent.charAt(2), '#').replace(parent.charAt(5), parent.charAt(2)).replace('#', parent.charAt(5)));
+            case 2: {   // Blank located at top right
+                children.add(parent.replace(parent.charAt(2), '#').replace(parent.charAt(1), parent.charAt(2)).replace('#', parent.charAt(1))); // Move Left
+                children.add(parent.replace(parent.charAt(2), '#').replace(parent.charAt(5), parent.charAt(2)).replace('#', parent.charAt(5))); // Move Down
                 break;
             }
-            case 3: {
-                children.add(parent.replace(parent.charAt(3), '#').replace(parent.charAt(0), parent.charAt(3)).replace('#', parent.charAt(0)));
-                children.add(parent.replace(parent.charAt(3), '#').replace(parent.charAt(4), parent.charAt(3)).replace('#', parent.charAt(4)));
-                children.add(parent.replace(parent.charAt(3), '#').replace(parent.charAt(6), parent.charAt(3)).replace('#', parent.charAt(6)));
+            case 3: {   // Blank located at middle left
+                children.add(parent.replace(parent.charAt(3), '#').replace(parent.charAt(0), parent.charAt(3)).replace('#', parent.charAt(0))); // Move Up
+                children.add(parent.replace(parent.charAt(3), '#').replace(parent.charAt(4), parent.charAt(3)).replace('#', parent.charAt(4))); // Move Right
+                children.add(parent.replace(parent.charAt(3), '#').replace(parent.charAt(6), parent.charAt(3)).replace('#', parent.charAt(6))); // Move Down
                 break;
             }
-            case 4: {
-                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(1), parent.charAt(4)).replace('#', parent.charAt(1)));
-                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(3), parent.charAt(4)).replace('#', parent.charAt(3)));
-                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(5), parent.charAt(4)).replace('#', parent.charAt(5)));
-                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(7), parent.charAt(4)).replace('#', parent.charAt(7)));
+            case 4: {   // Blank located at middle middle
+                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(1), parent.charAt(4)).replace('#', parent.charAt(1))); // Move Up
+                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(3), parent.charAt(4)).replace('#', parent.charAt(3))); // Move Left
+                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(5), parent.charAt(4)).replace('#', parent.charAt(5))); // Move Right
+                children.add(parent.replace(parent.charAt(4), '#').replace(parent.charAt(7), parent.charAt(4)).replace('#', parent.charAt(7))); // Move Down
                 break;
             }
-            case 5: {
-                children.add(parent.replace(parent.charAt(5), '#').replace(parent.charAt(2), parent.charAt(5)).replace('#', parent.charAt(2)));
-                children.add(parent.replace(parent.charAt(5), '#').replace(parent.charAt(4), parent.charAt(5)).replace('#', parent.charAt(4)));
-                children.add(parent.replace(parent.charAt(5), '#').replace(parent.charAt(8), parent.charAt(5)).replace('#', parent.charAt(8)));
+            case 5: {   // Blank located at middle right
+                children.add(parent.replace(parent.charAt(5), '#').replace(parent.charAt(2), parent.charAt(5)).replace('#', parent.charAt(2))); // Move Up
+                children.add(parent.replace(parent.charAt(5), '#').replace(parent.charAt(4), parent.charAt(5)).replace('#', parent.charAt(4))); // Move Left
+                children.add(parent.replace(parent.charAt(5), '#').replace(parent.charAt(8), parent.charAt(5)).replace('#', parent.charAt(8))); // Move Down
                 break;
             }
-            case 6: {
-                children.add(parent.replace(parent.charAt(6), '#').replace(parent.charAt(3), parent.charAt(6)).replace('#', parent.charAt(3)));
-                children.add(parent.replace(parent.charAt(6), '#').replace(parent.charAt(7), parent.charAt(6)).replace('#', parent.charAt(7)));
+            case 6: {   // Blank located at bottom left
+                children.add(parent.replace(parent.charAt(6), '#').replace(parent.charAt(3), parent.charAt(6)).replace('#', parent.charAt(3))); // Move Up
+                children.add(parent.replace(parent.charAt(6), '#').replace(parent.charAt(7), parent.charAt(6)).replace('#', parent.charAt(7))); // Move Right
                 break;
 
             }
-            case 7: {
-                children.add(parent.replace(parent.charAt(7), '#').replace(parent.charAt(4), parent.charAt(7)).replace('#', parent.charAt(4)));
-                children.add(parent.replace(parent.charAt(7), '#').replace(parent.charAt(6), parent.charAt(7)).replace('#', parent.charAt(6)));
-                children.add(parent.replace(parent.charAt(7), '#').replace(parent.charAt(8), parent.charAt(7)).replace('#', parent.charAt(8)));
+            case 7: {   // Blank located at bottom middle
+                children.add(parent.replace(parent.charAt(7), '#').replace(parent.charAt(4), parent.charAt(7)).replace('#', parent.charAt(4))); // Move Up
+                children.add(parent.replace(parent.charAt(7), '#').replace(parent.charAt(6), parent.charAt(7)).replace('#', parent.charAt(6))); // Move Left
+                children.add(parent.replace(parent.charAt(7), '#').replace(parent.charAt(8), parent.charAt(7)).replace('#', parent.charAt(8))); // Move Right
                 break;
             }
-            case 8: {
-                children.add(parent.replace(parent.charAt(8), '#').replace(parent.charAt(5), parent.charAt(8)).replace('#', parent.charAt(5)));
-                children.add(parent.replace(parent.charAt(8), '#').replace(parent.charAt(7), parent.charAt(8)).replace('#', parent.charAt(7)));
+            case 8: {   // Blank located at bottom right
+                children.add(parent.replace(parent.charAt(8), '#').replace(parent.charAt(5), parent.charAt(8)).replace('#', parent.charAt(5))); // Move Up
+                children.add(parent.replace(parent.charAt(8), '#').replace(parent.charAt(7), parent.charAt(8)).replace('#', parent.charAt(7))); // Move Left
                 break;
             }
         }
